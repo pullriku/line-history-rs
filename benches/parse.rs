b478b81798ac84ab239a9f95f8e8edbb3606a519
pullriku <riku3dno.1@gmail.com>
2025-02-26 11:54:57 +0900

    owned bench

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,4 +1,5 @@
 use criterion::{criterion_group, criterion_main};
+use line_history::history::ignore_errors;
 
 fn bench_parse(criterion: &mut criterion::Criterion) {
     let mut group = criterion.benchmark_group("size");
@@ -8,6 +9,22 @@ fn bench_parse(criterion: &mut criterion::Criterion) {
         b.iter(|| {
             let _history = line_history::parse::parse_history(&src);
         })
+    });
+
+    group.bench_function("parse into_owned", |b| {
+        b.iter(|| {
+            let history = line_history::parse::parse_history(&src);
+            let history = ignore_errors(history);
+            let _ = history.into_owned();
+        });
+    });
+
+    group.bench_function("clone into_owned", |b| {
+        let history = line_history::parse::parse_history(&src);
+        let history = ignore_errors(history);
+        b.iter(|| {
+            let _ = history.clone().into_owned();
+        });
     });
 }
 
